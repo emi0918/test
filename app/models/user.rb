42b41e56ad9504a266be460f617da00c054e5c50
Naggi <naggitapdance@gmail.com>
2016-03-26 14:25:58 +0900

    余計な記述を消した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-# validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
-
-# validates :email,
-# presence: { message:  "メールアドレスを入力してください"},
-# uniqueness: { message: "既に登録済みのメールアドレスです。"}
-
 end
