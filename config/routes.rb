2ee7d87caac844675be622e3a2a0dfcf873ad47d
emi0918 <emi.m0918@gmail.com>
2016-03-28 15:44:57 +0900

    housingのページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users
   root'home#index'
+  resources :home, only: [:index]
+  resources :housing, only: [:index]
+  resources :dashboard, only: [:index]
   get '/notes/new' =>'notes#new'
   post '/notes' => 'notes#create'
   get '/pro' => 'home#pro'
   get '/policy'=>'home#policy'
   get '/signage' =>'home#signage'
   get '/term' => 'home#term'
-  resources :users, only: [:show, :new, :create]
 
-match '*path' => 'application#error404', via: :all
+# match '*path' => 'application#error404', via: :all
 
 end
