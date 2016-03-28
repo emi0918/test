285ed30ff95b9f6f24dc004d7f2e2f8c1537ce6d
emi0918 <emi.m0918@gmail.com>
2016-03-28 15:50:19 +0900

    変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
 
-  rescue_from ActiveRecord::RecordNotFound,ActionController::RoutingError, with: :error404
-  rescue_from Exception, with: :render_500
+  # rescue_from ActiveRecord::RecordNotFound,ActionController::RoutingError, with: :error404
+  # rescue_from Exception, with: :render_500
 
 
-  def error_404(exception = nil)
-   render 'error404', status: 404, formats: [:html]
-  end
+  # def error_404(exception = nil)
+  #  render 'error404', status: 404, formats: [:html]
+  # end
 
 
-  def error_500(exception = nil)
-    logger.error [e,*e.backtrace].join ("¥n")
-     render 'error500', status: 500, formats: [:html]
-  end
+  # def error_500(exception = nil)
+  #   logger.error [e,*e.backtrace].join ("¥n")
+  #    render 'error500', status: 500, formats: [:html]
+  # end
 end
