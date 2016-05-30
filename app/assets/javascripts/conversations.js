3d1ca88c8bab424186d5e99278cc62f2c88f3167
emi0918 <emi.m0918@gmail.com>
2016-05-30 22:15:35 +0900

    変更①

---
+++ app/assets/javascripts/conversations.js
@@ -0,0 +1,12 @@
+// var ready;
+
+// ready = function(){
+//     // enable chosen js
+//     $('.chosen-select').chosen({
+//         no_results_text: 'No results matched'
+//     });
+// }
+
+// // if using turbolinks
+// $(document).on("page:load",ready);
+
