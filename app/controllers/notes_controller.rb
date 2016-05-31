7698c87a278d11f8ee2e295818331365434e2ad8
emi0918 <emi.m0918@gmail.com>
2016-05-31 20:23:28 +0900

    notes 変更

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,17 +7,24 @@ include ApplicationHelper
   # GET /notes.json
   def index
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 3d6c39304d2456958ddf86dc587b95104b9d9f56
+=======
+
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
     @notes = current_user.notes.all
        @notes = Note.page(params[:page]).per(3).order(:id)
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 3d6c39304d2456958ddf86dc587b95104b9d9f56
+=======
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
   # GET /notes/1
   # GET /notes/1.json
 def show
@@ -34,35 +41,23 @@ end
 def new
     @note = Note.new
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
   end
 
 
   # GET /notes/1/sedit
   def edit
     @note = Note.find(params[:id])
+correct_user
   end
-
-def description
- @note = Note.find(params[:id])
-              @note.update_attributes (params[:note])
-      if @note.update(note_params)
-        redirect_to @note, notice: '編集完了しました'
-      else
-        render :edit
-      end
-    end
-
-
-def images
-      @note = Note.new
-end
-
 
 
 
   # POST /notes
   # POST /notes.json
-  def create
+    def create
     @note = Note.new(note_params)
 
      file=params[:note][:image_1]
@@ -78,9 +73,11 @@ end
 
 
 
+
   # PATCH/PUT /notes/1
   # PATCH/PUT /notes/1.json
   def update
+    correct_user
         @note = Note.find(params[:id])
               @note.update_attributes (params[:note])
       if @note.update(note_params)
@@ -91,14 +88,45 @@ end
     end
 
 
+
   # DELETE /notes/1
   # DELETE /notes/1.json
 
+
   def destroy
+    correct_user
     @note.destroy
+<<<<<<< HEAD
      redirect_to notes_url, notice: '掲載削除しました'
 =======
 >>>>>>> 3d6c39304d2456958ddf86dc587b95104b9d9f56
+=======
+    redirect_to root_path
+  end
+
+end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+        @note = current_user.notes.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def note_params
+      params.require(:note).permit(:title, :user_name, :content, :price, :image_1, :image_2, :image_3, :category, :rule,:user_id)
+    end
+       def user_params
+      params.require(:user, :note).permit(:user_name, :profile_pic, :profile, :area, :email)
+    end
+
+   def correct_user
+    note = Note.find(params[:id])
+    if !current_user?(note.user)
+      redirect_to root_path
+    end
+end
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
   end
 
 
@@ -178,31 +206,22 @@ end
     end
 end
 <<<<<<< HEAD
+<<<<<<< HEAD
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
       @note = Note.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def note_params
-      params.require(:note).permit(:title, :user_name, :content, :price, :image_1, :image_2, :image_3, :category, :rule)
-    end
-       def user_params
-      params.require(:user, :note).permit(:user_name, :profile_pic, :profile, :area, :email)
-    end
-
-   def correct_user
-    note = Note.find(params[:id])
-    if !current_user?(note.user)
-      redirect_to root_path
-    end
-end
 =======
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
 
 
 
+
+<<<<<<< HEAD
 >>>>>>> 3d6c39304d2456958ddf86dc587b95104b9d9f56
+=======
+>>>>>>> 3a2a46bc5ee5caab923671d91b7927b7439c96db
 
 
 
