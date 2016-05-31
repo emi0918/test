4e725f171624d23eb9ebcf78074ad397e7cecfac
emi0918 <emi.m0918@gmail.com>
2016-05-31 20:26:51 +0900

    こみ

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class DashboardController < ApplicationController
+=======
+ class DashboardController < ApplicationController
+>>>>>>> responsive_seekle
   before_action :authenticate_user!
 
   helper_method :current_user, :logged_in?
@@ -8,12 +12,17 @@ class DashboardController < ApplicationController
     @notes = @user.notes
     @user_name = User.find(params[:id])
     @area = User.find(params[:id])
+<<<<<<< HEAD
   end
+=======
+   end
+>>>>>>> responsive_seekle
 
 private
 
 def current_user
   return unless session[:user_id]
+<<<<<<< HEAD
   @current_user ||=User.find(session[:user_id])
 end
 
@@ -21,8 +30,20 @@ def logged_in?
   !!session[:user_id]
 end
 
+=======
+ @current_user ||=User.find(session[:user_id])
+ end
+
+def logged_in?
+  !!session[:user_id]
+end
+
+>>>>>>> responsive_seekle
 
 end
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> responsive_seekle
