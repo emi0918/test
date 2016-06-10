b47ff14acfce11b018eee19e999363ed652ea0d2
emi0918 <emi.m0918@gmail.com>
2016-06-10 12:28:37 +0900

    facebook omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,27 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    callback_from :facebook
-  end
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
 
-  def fa
-
- private
-    def callback_from(provider)
-      provider = provider.to_s
-      @user = User.find_for_oauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        cookies.permanent[:xxx_logined] = { value: @user.created_at }
-        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-        sign_in_and_redirect @user, event: :authentication
-      else
-        if provider == 'twitter'
-          session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-        else
-          session["devise.facebook_data"] = request.env["omniauth.auth"]
-        end
-        redirect_to users_path
-      end
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to root_path
     end
+  end
 end
+
