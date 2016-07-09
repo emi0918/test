526315ed7c2d321c2caddb41e94ada5c0efd03df
emi0918 <emi.m0918@gmail.com>
2016-07-09 13:19:43 +0900

    Update sessions_controller.rb

--- app/controllers/providers/sessions_controller.rb
+++ app/controllers/providers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Providers::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
-
+layout "providers_layout"
   # GET /resource/sign_in
   # def new
   #   super
