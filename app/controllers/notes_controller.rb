36860544fa897aadc3cb33f8f4326420c95e86c6
森川恵美 <emi.m0918@gmail.com>
2016-07-11 14:51:54 +0900

    new_cahnge

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,17 +1,17 @@
 class NotesController < ApplicationController
-   before_action :correct_user, only: [:edit, :update]
-     before_action :set_note, only: [:show, :edit, :update, :destroy, :profile]
-       before_action :authenticate_user!, only:[:profile]
-       before_action :authenticate_provider!, only:[:index, :new, :edit, :create]
-include ApplicationHelper
+ before_action :correct_user, only: [:edit, :update]
+ before_action :set_note, only: [:show, :edit, :update, :destroy, :profile]
+ before_action :authenticate_user!, only:[:profile]
+ before_action :authenticate_provider!, only:[:index, :new, :edit, :create]
+ include ApplicationHelper
 
 
   # GET /notes
   # GET /notes.json
   def index
     @notes = current_provider.notes.all
-  @notes = Note.includes(:provider).page(params[:page]).per(3).order(:id)
-   render :layout => 'providers_layout.html'
+    @notes = Note.includes(:provider).page(params[:page]).per(3).order(:id)
+    render :layout => 'providers_layout.html'
   end
 
   # GET /notes/1
@@ -19,12 +19,12 @@ include ApplicationHelper
   def show
    @notes = Note.includes(:provider).all
    @note = Note.find(params[:id])
-  end
+ end
 
-  def search
+ def search
    @notes = Note.includes(:provider).page(params[:page]).per(6).order(:id)
 
-  end
+ end
 
  def profile
    @notes = Note.includes(:provider).all
@@ -34,8 +34,8 @@ include ApplicationHelper
 
   def new
    @note = Note.new
-    render :layout => 'providers_layout.html'
-  end
+   render :layout => 'providers_layout.html'
+ end
 
   # GET /notes/1/sedit
   def edit
@@ -46,33 +46,33 @@ include ApplicationHelper
 
   # POST /notes
   # POST /notes.json
-    def create
+  def create
 
     @note = current_provider.notes.build(note_params)
 
-     file=params[:note][:image_1]
+    file=params[:note][:image_1]
     @note.set_image_1(file)
-   
-   if @note.save
-       redirect_to @note
+
+    if @note.save
+     redirect_to @note
 #redirect_to @note で作成されたものが表示される
-      else
-         render :new
-      end
-    end
+else
+ render :new
+end
+end
 
   # PATCH/PUT /notes/1
   # PATCH/PUT /notes/1.json
   def update
 
-        @note = Note.find(params[:id])
+    @note = Note.find(params[:id])
 
-      if @note.update(note_params)
-        redirect_to @note, notice: '編集完了しました'
-      else
-        render :edit
-      end
+    if @note.update(note_params)
+      redirect_to @note, notice: '編集完了しました'
+    else
+      render :edit
     end
+  end
 
   # DELETE /notes/1
   # DELETE /notes/1.json
@@ -83,23 +83,21 @@ include ApplicationHelper
   end
 
 
-
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-         @note = Note.find(params[:id])
-    end
+     @note = Note.find(params[:id])
+   end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
       params.require(:note).permit(:title, :user_name, :content, :price, :image_1, :image_2, :image_3, :category, :rule,:provider_id)
     end
-       def user_params
+    def user_params
       params.require(:user).permit(:user_name, :profile_pic, :profile, :area, :email)
     end
-end
+  end
 
- 
+
 
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,34 +1,23 @@
 class Note < ActiveRecord::Base
 	belongs_to :provider
-belongs_to :user
-		has_many :likes, dependent: :destroy
-has_many :liking_users, through: :likes, source: :user
+	belongs_to :user
+	has_many :likes, dependent: :destroy
+	has_many :liking_users, through: :likes, source: :user
 
 	validates :image_1, presence: false
 	validates :image_2, presence: false
 	validates :image_3, presence: false
-  validates :title, presence:  true
-  validates :category, presence: true
-  validates :content, presence: true
-  validates :price, presence: true
-  validates :rule, presence: false
+	validates :title, presence:  true
+	validates :category, presence: true
+	validates :content, presence: true
+	validates :price, presence: true
+	validates :rule, presence: false
 
 	def set_image_1(file)
-		 if !file.nil?
+		if !file.nil?
 			file_name = file.original_filename
 			File.open("public/note_images/#{file_name}", 'wb'){|f| f.write(file.read)}
 			self.image_1 = file_name
 		end
 	end
 end
-
-
-
-
-
-
-
-
-
-
-
