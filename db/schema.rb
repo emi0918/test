c8e212d0c28ca245424fff1cdb3fce7ebc88b68c
Emi Morikawa <emi.m0918@gmail.com>
2016-07-16 19:41:11 +0900

    変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712124746) do
+ActiveRecord::Schema.define(version: 20160521082610) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160712124746) do
     t.text     "image_3",     limit: 65535
     t.string   "category",    limit: 255
     t.text     "rule",        limit: 65535
+    t.string   "user_id",     limit: 255
     t.string   "provider_id", limit: 255
   end
 
@@ -113,6 +114,11 @@ ActiveRecord::Schema.define(version: 20160712124746) do
   add_index "providers", ["email"], name: "index_providers_on_email", unique: true, using: :btree
   add_index "providers", ["reset_password_token"], name: "index_providers_on_reset_password_token", unique: true, using: :btree
 
+  create_table "service_providers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
@@ -133,7 +139,6 @@ ActiveRecord::Schema.define(version: 20160712124746) do
     t.integer  "uid",                    limit: 8
     t.string   "provider",               limit: 255
     t.string   "token",                  limit: 255
-    t.string   "unique_session_id",      limit: 20
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
