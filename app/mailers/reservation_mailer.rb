bc6d6abc267fcda808a8d70d0e9bb96aa29c5ea4
emi0918 <emi.m0918@gmail.com>
2016-08-02 15:27:49 +0900

    Update reservation_mailer.rb

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,6 +1,8 @@
 class ReservationMailer < ApplicationMailer
  default from: "from@example.com"
 
+
+#provider側に予約が来たよのメールを送信
   def reservation_email(provider, reservation)
   	 @provider = provider
       @title = reservation.note.title
@@ -9,4 +11,17 @@ class ReservationMailer < ApplicationMailer
       @message=reservation.message
     mail to:  reservation.note.provider.email, subject: "【Seekle】掲載中のサービスに予約が来ています！"
   end
+
+
+#user側に予約完了確認のメールを送信
+  def myreservation_email(user, reservation)
+  	 @provider = reservation.note.provider.name
+      @title = reservation.note.title
+      @date = reservation.date
+      @name= reservation.name
+      @message=reservation.message
+    mail to:  reservation.user.email, subject: "【Seekle】仮予約が完了しました！(予約内容確認)"
+  end
+
+
 end
