34a6ea0d895cc4a3ecc68667ea0fd1f5e75159f3
emi0918 <emi.m0918@gmail.com>
2016-08-30 13:24:01 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ActiveRecord::Base
 	has_many :category_notes
-    has_many :notes, through: :category_notes
+    belongs_to :notes
     has_many :detail_categories
+    belongs_to :note_revisions
 
 end
 
