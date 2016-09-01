9ee9af9a3cfbbffdbd8d4cd2c2c02bd162d7b046
emi0918 <emi.m0918@gmail.com>
2016-09-01 21:29:26 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
+
+def index
+ @note_id = params[:id]
+   @note1 = Note.find(4) #全てのレコードを取得する
+@note2 = Note.find(5)
+@note3 = Note.find(6)
+@note4 = Note.find(7)
+
+ end
+
 end
 
