f7885a3d31df6cf037f5e7e5c1b72ee1bd8b0fe0
emi0918 <emi.m0918@gmail.com>
2016-09-01 21:31:33 +0900

    Update reservation_mailer.rb

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -5,8 +5,15 @@ class ReservationMailer < ApplicationMailer
 #provider側に予約が来たよのメールを送信
   def reservation_email(provider, reservation)
   	 @provider = provider
-
+   @workdate1 = reservation.workdate1
+   @workdate2 = reservation.workdate2
+   @workdate3 = reservation.workdate3
+   @worktime1 = reservation.worktime1
+   @worktime2 = reservation.worktime2
+   @worktime3 = reservation.worktime3
   @reservations = Reservation.includes(:note).all
+  @reservations = Reservation.includes(:note_revisions).all
+@note= reservation.note
       @date = reservation.date
       @name= reservation.name
       @message=reservation.message
@@ -17,13 +24,22 @@ class ReservationMailer < ApplicationMailer
 #user側に予約完了確認のメールを送信
   def myreservation_email(user, reservation)
   	 @provider = reservation.note.provider.name
-   @reservations = Reservation.includes(:note).all
+       @workdate1 = reservation.workdate1
+   @workdate2 = reservation.workdate2
+   @workdate3 = reservation.workdate3
+   @worktime1 = reservation.worktime1
+   @worktime2 = reservation.worktime2
+   @worktime3 = reservation.worktime3
+@note= reservation.note
+     @reservations = Reservation.includes(:note).all
+     @reservations = Reservation.includes(:note_revisions).all
+
       @date = reservation.date
       @name= reservation.name
       @message=reservation.message
     mail to:  reservation.user.email, subject: "【Seekle】仮予約が完了しました！(予約内容確認)"
-  end
 
+  end
 
 end
 
