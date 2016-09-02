eeea7b4239a66eb19e5de91462ad6328537f75f2
emi0918 <emi.m0918@gmail.com>
2016-09-02 19:14:18 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 
 def index
+
  @note_id = params[:id]
-   @note1 = Note.find(4) #全てのレコードを取得する
-@note2 = Note.find(5)
-@note3 = Note.find(6)
-@note4 = Note.find(7)
+   @note1 = Note.find(236) #全てのレコードを取得する
+@note2 = Note.find(236)
+@note3 = Note.find(236)
+@note4 = Note.find(236)
+
+ render :layout => 'top_home.html'
 
  end
 
