42101526b89d19997308f37849a2be7b071318c5
emi0918 <emi.m0918@gmail.com>
2016-09-02 19:44:41 +0900

    Update note.rb

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,16 +1,14 @@
 class Note < ActiveRecord::Base
 	has_many :note_revisions, dependent: :destroy
 	 accepts_nested_attributes_for :note_revisions,allow_destroy: true
-has_many :note_images,  dependent: :destroy
- accepts_nested_attributes_for :note_images,allow_destroy: true
-
-	belongs_to :provider, dependent: :destroy
-	belongs_to :user, dependent: :destroy
-	belongs_to  :mailboxer_conversations, dependent: :destroy
+    has_many :note_images,  dependent: :destroy
+     accepts_nested_attributes_for :note_images,allow_destroy: true
+	belongs_to :provider
+	belongs_to :user
+	belongs_to  :mailboxer_conversations
     belongs_to :category
 	has_many :reservations, :dependent => :destroy
-	has_many :likes, dependent: :destroy
-	has_many :liking_users, through: :likes, source: :user
+	
    has_many :reviews, dependent: :destroy
 has_many :mailboxer_conversations
 
@@ -23,4 +21,5 @@ has_many :mailboxer_conversations
 validates :category_id, presence: true
 
 end
+
 
