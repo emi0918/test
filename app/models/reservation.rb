4f58f4082ac8f32251a343c758579d27f0e9ee92
emi0918 <emi.m0918@gmail.com>
2016-09-02 19:45:23 +0900

    Update reservation.rb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,18 @@
 class Reservation < ActiveRecord::Base 
+
+
 	belongs_to :user
 	belongs_to :note
 	belongs_to :provider
 
+
     validates :name, presence:  true
 	validates :phone_number, presence: true
 	validates :message, presence: true
-	validates :date, presence: true
+	
+
+	
 
 end
+
+
