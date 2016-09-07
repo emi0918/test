aa56f4e57048028a1b5979379cb5563dbf31a292
emi0918 <emi.m0918@gmail.com>
2016-09-07 16:00:17 +0900

    Update category_controller.rb

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,11 +5,7 @@ class CategoryController < ApplicationController
     @notes = Note.find(:all, :conditions => ["category_id = ?", params[:id]])
     end
 
-    def housing
-    	 @housecategories =  Category.where(:parent_id => (1))
-			@category= Category.find_by(name: params[:category])
-			@notes = Note.where(category_id: @category_id).order("created_at DESC")
-	end
+
 
 
 end
