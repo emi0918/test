8247c0e2e1c042792994854f48390d48aa5f5602
emi0918 <emi.m0918@gmail.com>
2016-09-08 20:30:29 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,18 @@
 
+require "awesome_nested_set"
+
+
+
 class Category < ActiveRecord::Base
 
     belongs_to :notes
+
+  acts_as_nested_set
 
 
 end
 
 # throughオプションによりcategory_notes経由でnotesにアクセスできるようになる
 # category.notesでプロジェクトにアクセスができる
+
 
