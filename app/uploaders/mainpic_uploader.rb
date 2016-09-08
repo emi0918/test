c7ee63ea859a637729329d9f23b632d51b143830
emi0918 <emi.m0918@gmail.com>
2016-09-08 21:54:11 +0900

    Update mainpic_uploader.rb

--- app/uploaders/mainpic_uploader.rb
+++ app/uploaders/mainpic_uploader.rb
@@ -1,15 +1,14 @@
 class MainpicUploader < CarrierWave::Uploader::Base
 
  include CarrierWave::RMagick
-
-  process :resize_to_limit => [700, 700]
+  process :resize_to_limit => [1000, 1000]
 
   # 保存形式をJPGにする
   process :convert => 'jpg'
 
-  # サムネイルを生成する設定
+   # サムネイルを生成する設定
   version :thumb do
-    process :resize_to_limit => [300, 300]
+    process :resize_to_limit => [900, 900]
   end
 
   # jpg,jpeg,gif,pngしか受け付けない
@@ -18,19 +17,12 @@ class MainpicUploader < CarrierWave::Uploader::Base
   end
 
 
- # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
 
  # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
  # 日付(20131001.jpgみたいなファイル名)で保存する
-  def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-    name.downcase
+   def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
   end
-
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -38,8 +30,9 @@ class MainpicUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "public/uploads/user"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
 
  protected
   def secure_token
@@ -79,4 +72,3 @@ class MainpicUploader < CarrierWave::Uploader::Base
   # end
 
 end
-
