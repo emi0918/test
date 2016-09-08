c4bd809a40fad7d6584fabb6ea6131ccf4780334
emi0918 <emi.m0918@gmail.com>
2016-09-08 19:09:54 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,9 +4,6 @@ class Category < ActiveRecord::Base
     belongs_to :notes
 
 
-acts_as_nested_set parent_column: :parent_id, left_column: :lft, right_column: :rght
-
-
 end
 
 # throughオプションによりcategory_notes経由でnotesにアクセスできるようになる
