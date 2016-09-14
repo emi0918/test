d0fc8fba950e3877f791d6cdf72cc84edcf1afd5
emi0918 <emi.m0918@gmail.com>
2016-09-14 10:57:35 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,18 @@ class User < ActiveRecord::Base
 
 
 has_many :reviews, dependent: :destroy
-
-
-  has_many :reservations, dependent: :destroy
+has_many :reservations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable, :omniauthable # other devise options
 
+  validates :name, length:{maximum:10}, presence:true
   validates :profile, length:{maximum:1000}, presence:false
   validates :area, presence:false
 
   acts_as_messageable
+  
 
   def mailboxer_name
     self.name
