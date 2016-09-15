1bf6b8f784df3918fc37803501bb85f9b6b59818
emi0918 <emi.m0918@gmail.com>
2016-09-15 22:23:02 +0900

    Update store_info.rb

--- app/models/store_info.rb
+++ app/models/store_info.rb
@@ -2,10 +2,7 @@ class StoreInfo < ActiveRecord::Base
 		belongs_to :provider
 
 
-	validates :payment, presence: true
-	validates :hours, presence: true
-	validates :prefecture, presence: true
-	validates :holiday, presence: true
+
 
 end
 
