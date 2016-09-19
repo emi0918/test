8741e84f7936dad6ab2e1271f7df91beee7852ef
emi0918 <emi.m0918@gmail.com>
2016-09-19 16:15:23 +0900

    Update category_controller.rb

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,7 +5,34 @@ class CategoryController < ApplicationController
     @notes = Note.find(:all, :conditions => ["category_id = ?", params[:id]])
     end
 
+    def housing
+       
+
+    	@maincategories =Category.where(:id => [(1),(2),(3),(4),(5)]  )
+     	@parentcategories =Category.find_by(name: params[:category]).id
+     
+      @categories =  Category.where(:parent_id => (1))
+     
+	   
+	end
 
 
+    def event
+      @maincategories =Category.where(:id => [(1),(2),(3),(4),(5)]  )
+      @parentcategories =Category.find_by(name: params[:category]).id
+      @categories =  Category.where(:parent_id => (2))
+  end
 
+    def lesson
+      @maincategories =Category.where(:id => [(1),(2),(3),(4),(5)]  )
+      @parentcategories =Category.find_by(name: params[:category]).id
+      @categories =  Category.where(:parent_id => (3))
+    end
+
+    def health
+      @maincategories =Category.where(:id => [(1),(2),(3),(4),(5)]  )
+      @parentcategories =Category.find_by(name: params[:category]).id
+      @categories =  Category.where(:parent_id => (4))
+    end
 end
+
