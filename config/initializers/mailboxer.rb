b7ee64fc0990aa94c4a628fb8edd3a91928f72f9
emi0918 <emi.m0918@gmail.com>
2016-09-25 16:46:48 +0900

    Update mailboxer.rb

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,14 +1,14 @@
 Mailboxer.setup do |config|
 
   #Configures if you application uses or not email sending for Notifications and Messages
-  config.uses_emails = false
+  config.uses_emails = true
 
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "no-reply@mailboxer.com"
+  config.default_from = "seekle.info@gmail.com"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
-  config.name_method = :mailboxer_name
+  config.name_method = :name
 
   #Configures if you use or not a search engine and which one you are using
   #Supported engines: [:solr,:sphinx]
