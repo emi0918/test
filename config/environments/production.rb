f71fab73811b2259815275582cf08d6eaa3de7f6
emi0918 <emi.m0918@gmail.com>
2016-10-03 15:09:30 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,10 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
+config.action_controller.consider_all_requests_local= false
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
