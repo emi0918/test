6f18659b8db6182eb3db1065042aac182fa82310
emi0918 <emi.m0918@gmail.com>
2016-10-03 15:13:04 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,9 @@ class ApplicationController < ActionController::Base
    helper_method :mailbox, :conversation
 
     protect_from_forgery with: :exception
-
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_404
+rescue_from ActionController::RoutingError, with: :render_404
+rescue_from Exception, with: :render_500
 
   def render_404(exception = nil)
     if exception
@@ -20,13 +21,9 @@ class ApplicationController < ActionController::Base
     render template: "errors/error_404", status: 404, layout: 'application'
   end
 
-
-  def render_500(exception = nil)
-    if exception
-      logger.info "Rendering 500 with exception: #{exception.message}"
-    end
-    render template: "errors/error_500", status: 500, layout: 'application'
-  end
+def render500
+  render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+end
 
 
     #protect_from_forgery
