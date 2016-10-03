3b99131daf3d5a10354991f0c6adbaf810a6dd48
emi0918 <emi.m0918@gmail.com>
2016-10-03 14:07:47 +0900

    Update mailboxer.rb

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -8,12 +8,14 @@ Mailboxer.setup do |config|
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
-  config.name_method = :name
+  config.name_method = :mailboxer_name
 
   #Configures if you use or not a search engine and which one you are using
   #Supported engines: [:solr,:sphinx]
   config.search_enabled = false
   config.search_engine = :solr
+
+
 
   #Configures maximum length of the message subject and body
   config.subject_max_length = 255
