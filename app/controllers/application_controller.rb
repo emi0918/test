0a789833ecb0b1c864c668764bfd01e27e49e7be
emi0918 <emi.m0918@gmail.com>
2016-10-06 21:23:52 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,17 @@ class ApplicationController < ActionController::Base
 
 
 
+ rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from Exception, with: :render_500
 
+    def render_404
+      render file: '/public/400', status: 404, layout: 'application', content_type: 'text/html'
+    end
 
-
-
+    def render_500https://seekle.jp/users/sign_up
+      render file: '/public/500', status: 500, layout: 'application', content_type: 'text/html'
+    end
 
 
     #protect_from_forgery
